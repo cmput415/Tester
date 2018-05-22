ef955b0921a02ed3312c19758754bce632224a79
Braedy Kuzma <braedy@ualberta.ca>
2018-05-22 17:07:58 -0600

    Building the toolchain.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,37 @@
-int main() {
+#include "TestHarness.h"
+
+#include "CLI11.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+int main(int argc, char **argv) {
+  CLI::App app{"CMPUT 415 testing utility"};
+
+  // Add the internal config file path option.
+  std::string configFilePath;
+  app.add_option("configFile", configFilePath, "Path to the tester JSON configuration file.")
+    ->required()->check(CLI::ExistingFile);
+
+  // Parse our command line options.
+  CLI11_PARSE(app, argc, argv);
+
+  // Open and read our json config file.
+  std::ifstream jsonFile(configFilePath);
+  JSON json;
+  jsonFile >> json;
+
+  try {
+    // Build our tester.
+    tester::Tester t(json);
+
+    // Run our tests.
+    t.runTests();
+  }
+  catch (const std::runtime_error &e) {
+    std::cout << "Test harness error: " << e.what() << '\n';
+  }
+
   return 0;
 }
