aa5b8df678b88002da1ea06c786ae0997fdddbc7
Braedy Kuzma <braedy@ualberta.ca>
2018-05-31 11:04:38 -0600

    Fix linking error.

--- include/util.h
+++ include/util.h
@@ -12,7 +12,7 @@ using JSON = nlohmann::json;
 namespace tester {
 
 // Makes sure a JSON object contains a key. Throws an error if it does not.
-void ensureContains(const JSON &json, std::string name) {
+inline void ensureContains(const JSON &json, std::string name) {
   if (json.count(name) == 0)
     throw std::runtime_error(name + " missing from JSON.");
 }
