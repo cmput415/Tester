ee910c6491580e6479abc16a5bc3470cddf3e935
Braedy Kuzma <braedy@ualberta.ca>
2018-10-08 16:17:53 -0600

    Add condition classes.

--- include/analysis/spreadsheet/Utility.h
+++ include/analysis/spreadsheet/Utility.h
@@ -2,21 +2,61 @@
 #define TESTER_UTILITY_H
 
 #include <ostream>
+#include <string>
 
 namespace tester {
 
+// Forward declare cell so there's no circular import.
+class Cell;
+
+// Base class for conditions.
 class Condition {
 public:
+  // Construct with an operator.
+  Condition(std::string op) : op(std::move(op)) { }
+
   // Dump the condition.
   virtual void dump(std::ostream &os) = 0;
+
+protected:
+  // The operator.
+  const std::string op;
+};
+
+// Comparing against a constant int.
+class IntLiteralCondition : public Condition {
+public:
+  // Construct with operator and value.
+  IntLiteralCondition(std::string op, int value) : Condition(std::move(op)), value(value) { }
+
+  void dump(std::ostream &os) override;
+
+private:
+  // The value to compare against.
+  const int value;
+};
+
+// Comparing a constant against a cell's value.
+class CellCondition : public Condition {
+public:
+  // Construct with operator and value.
+  CellCondition(std::string op, const Cell &cell, int value)
+      : Condition(std::move(op)), cell(cell), value(value) { }
+
+  void dump(std::ostream &os) override;
+
+private:
+  // The cell to compare against.
+  const Cell &cell;
+
+  // The value to compare against.
+  const int value;
 };
 
 std::string idxToColName(size_t idx);
 
 std::string posToCellName(size_t col, size_t row);
 
-
-
-}
+} // End namespace tester
 
 #endif //TESTER_UTILITY_H
--- src/analysis/spreadsheet/Utility.cpp
+++ src/analysis/spreadsheet/Utility.cpp
@@ -1,5 +1,16 @@
 #include "analysis/spreadsheet/Utility.h"
 
+#include "analysis/spreadsheet/Cell.h"
+
+namespace tester {
+
+void IntLiteralCondition::dump(std::ostream &os) {
+  os << '"' << op << value << '"';
+}
+
+void CellCondition::dump(std::ostream &os) {
+  os << posToCellName(cell.getCol(), cell.getRow()) << '"' << op << value << '"';
+}
 
 std::string idxToColName(size_t idx) {
   std::string result;
@@ -15,4 +26,6 @@ std::string idxToColName(size_t idx) {
 
 std::string posToCellName(size_t col, size_t row) {
   return idxToColName(col) + std::to_string(row);
+}
+
 }
