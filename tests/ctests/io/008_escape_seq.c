8c8e3e0b614e0aec6c27a95ec9021e98a833c138
Justin Meimar <meimar@ualberta.ca>
2024-05-25 13:13:04 -0600

    New tests for tab and other escape characters

---
+++ tests/ctests/io/008_escape_seq.c
@@ -0,0 +1,6 @@
+
+int main() {
+	printf("\tafter the tab");
+}
+
+//CHECK_FILE:008_escape_seq.out
---
+++ tests/ctests/io/009_tab_newline.c
@@ -0,0 +1,8 @@
+int main() {
+
+    printf("\tabc\ndef\tehg\njki *END*");
+    
+    return 0;
+}
+
+// CHECK_FILE:009_tab_newline.out
