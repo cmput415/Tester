a764751b0fe4ef5c035c25beed34e50a92d2c907
Justin Meimar <meimar@ualberta.ca>
2024-06-17 13:51:38 -0600

    Add expected failure tests to the CI

--- tests/fail_tests/run.sh
+++ tests/fail_tests/run.sh
@@ -10,21 +10,21 @@ TEST_CONFIGS=(
 # we need to be in the test dir to run tests
 cd $SCRIPT_DIR
 
-all_passed=true
+all_failed=true
 
 for TEST_CONFIG in "${TEST_CONFIGS[@]}"; do
 
   $PROJECT_BASE/bin/tester $TEST_CONFIG --timeout 10
   status=$?
 
-  if [ $status -ne 0 ]; then
-    all_passed=false
+  if [ $status -eq 0 ]; then
+    all_failed=false
     echo "Tester failed test for config: $TEST_CONFIG" 
   fi
 done
 
 # run C tests
-if [ "$all_passed" = true ]; then
+if [ "$all_failed" = true ]; then
   exit 0
 else
   exit 1
